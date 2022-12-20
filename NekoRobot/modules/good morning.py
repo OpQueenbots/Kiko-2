15862d2dfbd1e163b7e11cd19675ee987a340d36
OpQueenbots <120937975+OpQueenbots@users.noreply.github.com>
2022-12-20 23:34:46 +0530

    Update good morning.py

--- NekoRobot/modules/good morning.py
+++ NekoRobot/modules/good morning.py
@@ -36,7 +36,7 @@ async def awake(event):
     NEKO = f" Welcome this beautiful morning with a smile on your face. I hope youll have a great day today. Wishing you a very good morning! {event.sender.first_name}"
     BUTTON = [
         [
-            Button.url("Meet Me Here🎀", "https://telegram.dog/Besties_XD"),
+            Button.url("💓 𝗛𝗲𝗮𝗿𝘁 💓", "https://t.me/WorldChattingFriendsWCF"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
