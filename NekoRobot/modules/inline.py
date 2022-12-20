1505b2e6207cfb1667182f54260e41fc89ae94c4
OpQueenbots <120937975+OpQueenbots@users.noreply.github.com>
2022-12-20 23:58:44 +0530

    Update inline.py

--- NekoRobot/modules/inline.py
+++ NekoRobot/modules/inline.py
@@ -71,28 +71,28 @@ def inlinequery(update: Update, _) -> None:
                 "description": "Look up a person/bot/channel/chat on @Intellivoid SpamProtection API",
                 "message_text": "Click the button below to look up a person/bot/channel/chat on @Intellivoid SpamProtection API using "
                 "username or telegram id",
-                "thumb_urL": "https://telegra.ph/file/0bf1b29555518a0d45948.jpg",
+                "thumb_urL": "https://te.legra.ph/file/f455ad70788ebcd93a709.jpg",
                 "keyboard": ".spb ",
             },
             {
-                "title": "Account info on Neko X",
+                "title": "Account info on Kiko",
                 "description": "Look up a Telegram account in Miku database",
-                "message_text": "Click the button below to look up a person in Neko X database using their Telegram ID",
-                "thumb_urL": "https://telegra.ph/file/0bf1b29555518a0d45948.jpg",
+                "message_text": "Click the button below to look up a person in Kiko database using their Telegram ID",
+                "thumb_urL": "https://te.legra.ph/file/f455ad70788ebcd93a709.jpg",
                 "keyboard": ".info ",
             },
             {
                 "title": "About",
-                "description": "Know about Neko X",
-                "message_text": "Click the button below to get to know about Neko X.",
-                "thumb_urL": "https://telegra.ph/file/0bf1b29555518a0d45948.jpg",
+                "description": "Know about Kiko",
+                "message_text": "Click the button below to get to know about Kiko.",
+                "thumb_urL": "https://te.legra.ph/file/f455ad70788ebcd93a709.jpg",
                 "keyboard": ".about ",
             },
             {
                 "title": "Anilist",
                 "description": "Search anime and manga on AniList.co",
                 "message_text": "Click the button below to search anime and manga on AniList.co",
-                "thumb_urL": "https://telegra.ph/file/0bf1b29555518a0d45948.jpg",
+                "thumb_urL": "https://te.legra.ph/file/f455ad70788ebcd93a709.jpg",
                 "keyboard": ".anilist ",
             },
         ]
@@ -195,7 +195,7 @@ def inlineinfo(query: str, update: Update, context: CallbackContext) -> None:
         [
             [
                 InlineKeyboardButton(
-                    text="Report Error", url="https://t.me/Besties_XD"
+                    text="Report Error", url="https://t.me/Kiko_Support"
                 ),
                 InlineKeyboardButton(
                     text="Search again",
@@ -208,7 +208,7 @@ def inlineinfo(query: str, update: Update, context: CallbackContext) -> None:
     results = [
         InlineQueryResultArticle(
             id=str(uuid4()),
-            thumb_url="https://telegra.ph/file/0bf1b29555518a0d45948.jpg",
+            thumb_url="https://te.legra.ph/file/f455ad70788ebcd93a709.jpg",
             title=f"User info of {html.escape(user.first_name)}",
             input_message_content=InputTextMessageContent(
                 text, parse_mode=ParseMode.HTML, disable_web_page_preview=True
@@ -233,12 +233,12 @@ def about(query: str, update: Update, context: CallbackContext) -> None:
     user = context.bot.get_chat(user_id)
     sql.update_user(user.id, user.username)
     about_text = f"""
-    [Neko X](https://t.me/NekoCuteBot)\n*Bot State:* `Alive`\n*Python:* `{python_version()}`\n*Pyrogram:* `{pyrover}`\n*Platform:* `{sys.platform}`\n*python-telegram-bot:* `v{str(__version__)}`
+    [Kiko](https://t.me/KikoManagement_Robot)\n*Bot State:* `Alive`\n*Python:* `{python_version()}`\n*Pyrogram:* `{pyrover}`\n*Platform:* `{sys.platform}`\n*python-telegram-bot:* `v{str(__version__)}`
     """
     kb = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(text="Support", url="https://t.me/Besties_XD"),
+                InlineKeyboardButton(text="Support", url="https://t.me/WorldChattingFriendsWCF"),
                 InlineKeyboardButton(
                     text="Sys Stats",
                     callback_data="pingCB",
@@ -252,8 +252,8 @@ def about(query: str, update: Update, context: CallbackContext) -> None:
             id=str(uuid4()),
             title="Alive",
             description="Check Bot's Stats",
-            thumb_url="https://telegra.ph/file/0bf1b29555518a0d45948.jpg",
-            photo_url="https://telegra.ph/file/0bf1b29555518a0d45948.jpg",
+            thumb_url="https://te.legra.ph/file/f455ad70788ebcd93a709.jpg",
+            photo_url="https://te.legra.ph/file/f455ad70788ebcd93a709.jpg",
             caption=about_text,
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=kb,
