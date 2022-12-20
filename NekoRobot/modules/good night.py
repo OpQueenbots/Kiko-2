0cba57c00e989736aaaeb380d2c861bc1452bccb
OpQueenbots <120937975+OpQueenbots@users.noreply.github.com>
2022-12-20 23:36:36 +0530

    Update good night.py

--- NekoRobot/modules/good night.py
+++ NekoRobot/modules/good night.py
@@ -36,7 +36,7 @@ async def awake(event):
     NEKO = f"Good night I hope tomorrow is the best day in your life. {event.sender.first_name}"
     BUTTON = [
         [
-            Button.url("Meet Me Here🎀", "https://telegram.dog/Besties_XD"),
+            Button.url("💓 𝗛𝗲𝗮𝗿𝘁 💓", "https://t.me/WorldChattingFriendsWCF"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
